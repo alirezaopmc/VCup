6c2aa800e668aa4cc83ebd768699c56ea94ad93d
SleepingJempa <alirezaopmc@gmail.com>
2021-01-31 14:47:53 +0330

    ++ Statement C ++ Testcases C

---
+++ src/99/Wave I - Warm Up/C/testcases/solver.py
@@ -0,0 +1,18 @@
+_i = int(input("which testcase? "))
+
+f = open(f'in/input{_i}.txt', 'r')
+
+lines = f.read().split('\n')
+
+r = lines[0]
+w = lines[1]
+
+ind = [-1] * 26
+for i in range(26):
+    ind[ord(r[i]) - ord('a')] = i
+
+ans = 0
+for ch in w:
+    ans += ind[ord(ch) - ord('a')] ** 2
+
+print(ans)
